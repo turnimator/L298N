bb28c4ba373f4e55981a151b1df08486422f2c3a
Jan Atle Ramsli <jaramsli@gmail.com>
2019-05-19 19:32:48 +0200

    Add documentation

---
+++ L298Nmotor.cpp
@@ -0,0 +1,145 @@
+
+uint8_t _throttle = 0;
+int8_t _steering_wheel = 90; // 90 degrees = straight ahead
+GearBox _gear = PARK; // Start in PARK. We don't want any accidents
+
+void L298Nmotor::setPwm()
+{
+	uint8_t throttle_left = _throttle;
+	uint8_t throttle_right = _throttle;
+	int8_t w = _steering_wheel - 90; // Center at 0
+	if (w < 0) { // Turning left
+		throttle_left = (throttle * abs(w)) / 100;
+	}
+	if (w > 0) { // Turning right
+		throttle_right = (throttle * abs(w)) / 100;
+	}
+	analogWrite(_motor_r_pwm_pin, throttle_left);
+	analogWrite(_motor_l_pwm_pin, throttle_right);
+}
+
+L298Nmotor::L298Nmotor(int pwmR=6, int in1L=38, int in2R=39, int pwmL=7, int in3L=40, int in4L=41)
+{
+	_motor_r_pwm_pin  = pwmR;
+	_motor_r_in1_pin = in1L;
+	_motor_r_in2_pin = in2R;
+
+	_motor_l_pwm_pin    = pwmL;
+	_motor_l_in3_pin = in3L;
+	_motor_l_in4_pin = in4L;
+	pinMode(_motor_r_pwm_pin, OUTPUT);
+	pinMode(_motor_r_in1_pin, OUTPUT);
+	pinMode(_motor_r_in2_pin, OUTPUT);
+
+	pinMode(_motor_l_pwm_pin, OUTPUT);
+	pinMode(_motor_l_in3_pin, OUTPUT);
+	pinMode(_motor_l_in4_pin, OUTPUT);
+
+	_throttle = 0;
+	_gear = PARK;
+}
+
+
+void L298Nmotor::turnlLeft(int8_t amt)
+{
+	steering_wheel += amt;
+	if (steering_wheel > 180) {
+		steering_wheel = 180;
+	}
+	setPwm();
+}
+
+void L298Nmotor::turnRight(int8_t amt)
+{
+	steering_wheel -= amt;
+	if (steering_wheel < 0) {
+		steering_wheel = 0;
+	}
+	setPwm();
+}
+
+void L298Nmotor::returnToCenter(int increment)
+{
+	if (_steering_wheel == 90){
+		return;
+	}
+	if (increment == 0){
+		_steering_wheel = 90;
+		setPwm();
+		return;
+	}
+	if (_steering_wheel < 90){
+		turnRight(increment);
+	}
+	if (_steering_wheel > 90){
+		turnLeft(increment);
+	}
+}
+
+
+void L298Nmotor::setThrottle(uint8_t newSpeed) {
+	_throttle = newSpeed;
+	setPwm();
+}
+
+void L298Nmotor::setGear(GearBox lever) {
+
+	__gear = lever;
+
+	switch (lever) {
+		case DRIVE: {
+				    digitalWrite(_motor_r_in1_pin, HIGH);
+				    digitalWrite(_motor_r_in2_pin, LOW);
+				    digitalWrite(_motor_l_in3_pin, HIGH);
+				    digitalWrite(_motor_l_in4_pin, LOW);
+			    }
+			    break;
+		case DRIVE_L: {
+				      digitalWrite(_motor_r_in1_pin, LOW);
+				      digitalWrite(_motor_r_in2_pin, HIGH);
+				      digitalWrite(_motor_l_in3_pin, HIGH);
+				      digitalWrite(_motor_l_in4_pin, LOW);
+			      }
+			      break;
+		case DRIVE_R: {
+				      digitalWrite(_motor_r_in1_pin, HIGH);
+				      digitalWrite(_motor_r_in2_pin, LOW);
+				      digitalWrite(_motor_l_in3_pin, LOW);
+				      digitalWrite(_motor_l_in4_pin, HIGH);
+			      }
+			      break;
+		case REVERSE: {
+				      digitalWrite(_motor_r_in1_pin, LOW);
+				      digitalWrite(_motor_r_in2_pin, HIGH);
+				      digitalWrite(_motor_l_in3_pin, LOW);
+				      digitalWrite(_motor_l_in4_pin, HIGH);
+			      }
+			      break;
+		case NEUTRAL:
+			      digitalWrite(_motor_r_in1_pin, LOW);
+			      digitalWrite(_motor_r_in2_pin, LOW);
+			      digitalWrite(_motor_l_in3_pin, LOW);
+			      digitalWrite(_motor_l_in4_pin, LOW);
+			      break;
+		case PARK:
+			      digitalWrite(_motor_r_in1_pin, LOW);
+			      digitalWrite(_motor_r_in2_pin, LOW);
+			      digitalWrite(_motor_l_in3_pin, LOW);
+			      digitalWrite(_motor_l_in4_pin, LOW);
+			      break;
+		default: {// NEUTRAL
+				 digitalWrite(_motor_r_in1_pin, LOW);
+				 digitalWrite(_motor_r_in2_pin, LOW);
+				 digitalWrite(_motor_l_in3_pin, LOW);
+				 digitalWrite(_motor_l_in4_pin, LOW);
+			 }
+			 break;
+	}
+
+	GearBox  L298Nmotor::getGear()
+	{
+		return _gear;
+	}
+
+}
+
---
+++ L298Nmotor.h
@@ -0,0 +1,33 @@
+enum GearBox {
+  PARK,
+  REVERSE,
+  NEUTRAL,
+  DRIVE,
+  DRIVE_L,
+  DRIVE_R
+};
+
+class L298Nmotor{
+private:
+	int __motor_r_pwm_pin;
+	int __motor_r_in1_pin;
+	int __motor_r_in2_pin;
+
+	int __motor_l_pwm_pin;
+	int __motor_l_in3_pin;
+	int __motor_l_in4_pin;
+
+	uint8_t _throttle = 0;
+	int8_t _steering_wheel = 90; // 90 degrees = straight ahead
+	GearBox _gear = PARK; // Start in PARK. We don't want any accidents
+
+	void setPwm();
+public:
+	L298Nmotor(int pwmR=6, int in1L=38, int in2R=39, int pwmL=7, int in3L=40, int in4L=41);
+	void turnlLeft(int8_t amt);
+	void turnRight(int8_t amt);
+	void returnToCenter(int increment); // Emulate caster angle forces, returning the steering wheel to center (increment=0 sets it dead center)
+	void setThrottle(uint8_t newSpeed);
+	void setGear(GearBox lever) ;
+};
+
